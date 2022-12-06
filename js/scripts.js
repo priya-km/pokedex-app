c006e15e7dafd0b4218f9b7832725a2bc8d8d43f
priya-km <priya.k.mah@gmail.com>
2022-12-05 19:42:56 -0500

    Updated scripts.js with the favoriteFood variable

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,6 @@ let size = 100;
 let doubleSize = size * 2;
 let minSize = (doubleSize * 2) - (size / 2);
 document.write(minSize);
+
+let favoriteFood = 'Cajun chicken alfredo';
+document.write(favoriteFood);
