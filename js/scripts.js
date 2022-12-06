fac1c5e6f47363023cb3285f92ea0a8d56181402
priya-km <priya.k.mah@gmail.com>
2022-12-05 19:37:23 -0500

    Added JS and CSS folders and files

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+let myName = 'Bob';
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
