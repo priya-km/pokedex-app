c75e14152266eb2a50f5cd75c477bc584b9b65ed
priya-km <priya.k.mah@gmail.com>
2022-12-11 23:57:44 -0500

    Added 3 objects and 3 keys for each object
    Created objects for the 3 starter pokemon of the paldea region. Also created 3 keys (name, height in meters, and type) for each of those pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-let pokemonList = [];
+let pokemonList = [
+    {
+        name: 'Fuecoco',
+        height: 0.4,
+        type: ['Fire']
+    },
+
+    {
+        name: 'Quaxly',
+        height: 0.5,
+        type: ['Water']
+    },
+
+    {
+        name: 'Sprigatito',
+        height: 0.4,
+        type: ['Grass']
+    }
+
+];
